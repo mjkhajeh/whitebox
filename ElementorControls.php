f2354d512325c068dbf4dabee4437a863b4dde96
MohammadJafar Khajeh <mjkhajehg@gmail.com>
2025-08-20 10:55:29 +0330

    Add button controls

--- ElementorControls.php
+++ ElementorControls.php
@@ -1545,8 +1545,8 @@ class ElementorControls {
 		];
 
 		// Default end controls
-		$date_types = apply_filters( 'mj\whitebox\elementor_controls\query_controls\date_types', Elementor::date_types(), $args );
-		$orderby_types = apply_filters( 'mj\whitebox\elementor_controls\query_controls\orderby', Elementor::orderby( $wc ), $args );
+		$date_types = apply_filters( 'mj\whitebox\elementor_controls\query_controls\date_types', Elementor::date_types( $args ), $args );
+		$orderby_types = apply_filters( 'mj\whitebox\elementor_controls\query_controls\orderby', Elementor::orderby( $wc, [], $args ), $args );
 
 		$end_controls = [
 			'query_date'		=> [
--- Utils/Elementor.php
+++ Utils/Elementor.php
@@ -17,7 +17,30 @@ class Elementor extends Utils {
 		return strpos( $string, '{{WRAPPER}}' ) !== false ? $string : "{{WRAPPER}} {$string}";
 	}
 
-	public static function date_types() {
+	public static function button_types( $args = [] ) {
+		$types = [
+			'primary'	=> esc_html_x( 'Primary', 'Button type', 'drplus' ),
+			'secondary'	=> esc_html_x( 'Secondary', 'Button type', 'drplus' ),
+			'gray'		=> esc_html_x( 'Gray', 'Button type', 'drplus' ),
+			'white'		=> esc_html_x( 'White', 'Button type', 'drplus' ),
+			'action'	=> esc_html_x( 'Action', 'Button type', 'drplus' ),
+			'bordered'	=> esc_html_x( 'Bordered', 'Button type', 'drplus' ),
+		];
+		$types = apply_filters( 'mj\whitebox\elementor_controls\button\types', $types, $args );
+		return $types;
+	}
+
+	public static function button_styles( $args = [] ) {
+		$styles = [
+			'normal'	=> esc_html_x( 'Normal', 'Button style', 'mj-whitebox' ),
+			'rounded'	=> esc_html_x( 'Rounded', 'Button style', 'mj-whitebox' ),
+			'circle'	=> esc_html_x( 'Circle', 'Button style', 'mj-whitebox' ),
+		];
+		$styles = apply_filters( 'mj\whitebox\elementor_controls\button\styles', $styles, $args );
+		return $styles;
+	}
+
+	public static function date_types( $args = [] ) {
 		return apply_filters( 'mj\whitebox\utils\elementor\date_types', [
 			'anytime'	=> esc_html__( 'All', 'mj-whitebox' ),
 			'today'		=> esc_html__( 'Past Day', 'mj-whitebox' ),
@@ -26,10 +49,10 @@ class Elementor extends Utils {
 			'quarter'	=> esc_html__( 'Past Quarter', 'mj-whitebox' ),
 			'year'		=> esc_html__( 'Past Year', 'mj-whitebox' ),
 			'exact'		=> esc_html__( 'Custom', 'mj-whitebox' ),
-		] );
+		], $args );
 	}
 
-	public static function orderby( $wc = false, $excludes = [] ) {
+	public static function orderby( $wc = false, $excludes = [], $args = [] ) {
 		if( !$wc ) {
 			$orderby = [
 				'post_date'		=> esc_html__( 'Date', 'mj-whitebox' ),
@@ -56,7 +79,7 @@ class Elementor extends Utils {
 			$orderby = parent::unset( $orderby, $excludes );
 		}
 
-		$orderby = apply_filters( 'mj\whitebox\utils\elementor\orderby', $orderby );
+		$orderby = apply_filters( 'mj\whitebox\utils\elementor\orderby', $orderby, $wc, $excludes, $args );
 
 		return $orderby;
 	}
